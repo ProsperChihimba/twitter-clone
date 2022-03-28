ce06cde339993f9eaca2881b77209332bf66ee8e
ProsperChihimba <prosperchihimba@gmail.com>
2022-03-28 23:37:38 +0300

    Add Feed in the home screen

--- data/tweets.ts
+++ data/tweets.ts
@@ -13,6 +13,34 @@ const tweets = [
         numberOfComments: 30,
         numberOfRetweets: 40,
         numberOfLikes: 70,
+    },
+    {
+        id: 't2',
+        user: {
+            id: 'u2',
+            username: 'abby',
+            name: 'Abby',
+            image: 'https://pbs.twimg.com/profile_images/1506373284736155659/ud04uunp_400x400.jpg'
+        },
+        createdAt: '2022-03-25T12:00:00.000Z',
+        content: 'It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.',
+        numberOfComments: 3,
+        numberOfRetweets: 4,
+        numberOfLikes: 7,
+    },
+    {
+        id: 't3',
+        user: {
+            id: 'u3',
+            username: 'chris',
+            name: 'Chriss',
+            image: 'https://pbs.twimg.com/profile_images/1506373284736155659/ud04uunp_400x400.jpg'
+        },
+        createdAt: '2022-03-18T12:00:00.000Z',
+        content: 'Simple tweet is here',
+        numberOfComments: 15,
+        numberOfRetweets: 20,
+        numberOfLikes: 37,
     }
 ];
 
